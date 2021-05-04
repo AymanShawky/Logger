588d08630e709eac55e72490e47138d8c661d899
aymanshawky <aymanshawky2009@gmail.com>
2021-05-04 04:19:55 +0200

    init commit

---
+++ Logger/LoggerManager.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+
+namespace Logger
+{
+    public class LoggerManager
+    {
+        private static readonly ILog log = LogManager.GetLogger("Orasqualia Log");
+        private LoggerManager() { }
+        public static void Log(string message, LogType logType = LogType.Info)
+        {
+            switch (logType)
+            {
+                case LogType.Info:
+                    log.Info(message);
+                    break;
+                case LogType.Error:
+                    log.Error(message);
+                    break;
+                case LogType.Warning:
+                    log.Warn(message);
+                    break;
+            }
+        }
+    }
+}
